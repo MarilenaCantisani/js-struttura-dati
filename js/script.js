f7cda7b74a4ecc2c1a068f3b2ea81946bdb2b7cb
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-22 15:07:37 +0200

    + Added description structure card

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/* -------------------------------------------------------------------------- */
+/*                                    MAGIC                                   */
+/* -------------------------------------------------------------------------- */
+
+
+//* Describe the structure of the playing card "magic"
+const cardMagic = {
+    name: "Bloodfire Colossus",
+    castingCost: ["6", "R", "R"],
+    typeCard: "Creature",
+    subtypeCard: "Giant",
+    symbolOfExpansion: {
+        reprintId: 9,
+        name: "Jump Start",
+        colorRarity: "golden"
+    },
+    skills: [
+        {
+            castingCost: ["R", "T"],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolores, quisquam esse impedit ex suscipit eos quidem nobis hic quis cupiditate et est provident eum fugit repellat veniam praesentium ipsum."
+        },
+        {
+            castingCost: ["W", "B", "T"],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolores, quisquam esse impedit ex suscipit eos quidem nobis hic quis cupiditate et est provident eum fugit repellat veniam praesentium ipsum."
+        }
+    ],
+    flavorText: {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolores, quisquam esse impedit ex suscipit eos quidem nobis hic quis cupiditate et est provident eum fugit repellat veniam praesentium ipsum",
+        author: "Lorem ipsum"
+    },
+    illustration: {
+        author: {
+            name: "Greg Staples"
+        },
+        imageSource: "https://cdn.cardsrealm.com/images/cartas/crop/10e-tenth-edition/bloodfire-colossus-191-med.jpeg?755"
+    },
+    numberOfCollection: "177/350",
+    strength: 13,
+    constitution: 13,
+    borderCardColor: "black",
+};
